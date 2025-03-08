a5e9211e2ab67acd3fa0ea4531ade839c708ae1b
KevinValladares06 <149841068+KevinValladares06@users.noreply.github.com>
2025-03-08 11:27:36 -0600

    informacion de noticias

--- data/pages/noticiasPage.js
+++ data/pages/noticiasPage.js
@@ -27,22 +27,22 @@ export const noticiasPage = {
             titulo: 'Equipo -2',
             articulos: [
                 {
-                    titulo: 'Titulo Articulo',
-                    imagen: 'url de la imagen /noticias/<equipo_x_y>.jpg',
-                    imagenDesc: 'descripción de la imágen',
-                    articulo: 'Lorem ipsum'
+                    titulo: "Captain Jack’s Crew: Innovación para la Exploración Submarina y la Conservación Marina",
+                    imagen: "url de la imagen /noticias/<equipo_x_y>.jpg",
+                    imagenDesc: "Una representación del equipo de Captain Jack’s Crew trabajando en su innovador sistema de reciclaje de oxígeno.",
+                    articulo: "El proyecto Captain Jack’s Crew busca revolucionar la exploración submarina mediante un innovador sistema que separa la molécula del agua para reciclar oxígeno, permitiendo extender el tiempo de buceo. El mayor desafío fue encontrar una fuente de energía eficiente para este proceso, pero el equipo logró superar esta barrera mediante un diseño optimizado. Más allá de la exploración, su invención tiene un impacto positivo en los arrecifes coralinos."
                 },
                 {
-                    titulo: 'Titulo Articulo',
-                    imagen: 'url de la imagen /noticias/<equipo_x_y>.jpg',
-                    imagenDesc: 'descripción de la imágen',
-                    articulo: 'Lorem ipsum'
+                    titulo: "Dowal Toursiops: Un Robot Autónomo para la Limpieza del Océano",
+                    imagen: "url de la imagen /noticias/<equipo_x_y>.jpg",
+                    imagenDesc: "Una ilustración de Dowal Toursiops, un robot flotando en aguas cristalinas mientras recoge desechos plásticos cerca de un arrecife de coral.",
+                    articulo: "El proyecto Dowal Toursiops busca reducir la contaminación marina mediante un robot autónomo equipado con sensores de inteligencia artificial que identifican y recogen basura sin afectar la vida marina. Inspirado en Wall-E, este innovador dispositivo tiene el potencial de limpiar los océanos de manera eficiente y sostenible. El mayor desafío del equipo fue el diseño 3D del robot. "
                 },
                 {
-                    titulo: 'Titulo Articulo',
-                    imagen: 'url de la imagen /noticias/<equipo_x_y>.jpg',
-                    imagenDesc: 'descripción de la imágen',
-                    articulo: 'Lorem ipsum'
+                    titulo: "Gator Byte: Tecnología y Biotecnología para un Océano Libre de Plásticos",
+                    imagen: "url de la imagen /noticias/<equipo_x_y>.jpg",
+                    imagenDesc: "Una ilustración de Gator Byte, un robot flotando en mar abierto mientras recoge residuos plásticos de una gran isla de basura.",
+                    articulo: "El proyecto Gator Byte se enfoca en combatir la contaminación oceánica mediante un robot diseñado para recolectar plásticos de las islas de basura y explorar el uso de bacterias especializadas en su degradación. Inspirado en Wall-E, el equipo enfrentó grandes desafíos en el diseño del robot y en la logística para implementar la biotecnología en el proceso de limpieza. Sin embargo, la colaboración y el trabajo en equipo hicieron de la experiencia un aprendizaje valioso."
                 },
             ]
         },
