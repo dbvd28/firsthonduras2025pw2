b96f92899f6e9463f2e0b4bcfc382e6190aea23e
FabyMencia <poncefabiola939@gmail.com>
2025-03-08 11:21:52 -0600

    Se agrega los articulo 2 del grupo 6

--- data/pages/noticiasPage.js
+++ data/pages/noticiasPage.js
@@ -96,7 +96,7 @@ export const noticiasPage = {
             articulos: [
                 {
                     titulo: 'Rest Wreck',
-                    imagen: 'public/imagenes_grupo6/AST_EAGLES_JUNIOR.jpeg',
+                    imagen: 'public/noticias/imagenes_grupo6/AST_EAGLES_JUNIOR.jpeg',
                     imagenDesc: 'Estudiantes de AST Eagles Junior',
                     articulo: 'Los estudiantes de AST se basaron su proyecto en el arrecife de coral en Tela, explican que el propósito de su proyecto es toman un poco de larva que se encuentra de corales sanos y se ponen dentro de corales que están por morir, esto lo que hace es que el coral que este por morir pueda volver a regenerarse. Esto es muy útil ya que así podríamos combatir la extinción o daño que reciben los corales debido a los turistas. Los estudiantes de AST Junior se sienten muy satisfechos con el trabajo que realizaron porque saben que es un proyecto beneficioso para la sociedad, además, aprendieron a trabajar en equipo y a explorar su creatividad.'
                 },
@@ -105,20 +105,12 @@ export const noticiasPage = {
             articulos: [
                 {
                     titulo: 'Submarino que utiliza luces ultrasonicas',
-                    imagen: 'public/imagenes_grupo6/ACTON_ACADEMY.jpeg',
+                    imagen: 'public/noticas/imagenes_grupo6/ACTON_ACADEMY.jpeg',
                     imagenDesc: 'Estudiantes de Acton Academy',
-                    articulo: 'Lorem ipsum'
+                    articulo: 'Los estudiantes de Acton Academy nos expusieron sobre lo perjudiciales que son las luces LED blancas afectan mas a los peces y las especies en el mar, este prototipo tiene como objetivo mantener la calidad de vida de los peces para que sigan reproduciéndose y no se extingan. La luz artificial afecta el ciclo reproductivo de los corales, también, en el caso los antunes, ellos se ven atraídos por la luz artificial lo que hace que grandes cantidades de ellos se agrupen causando la sobrepesca. En el prototipo que ellos presentan nos comentan que lo muestran con Arduino y utilizando luces ultrasónicas. El prototipo tiene una forma de submarino por lo que cuando es lanzado al océano tiene el propósito de que cada vez que se acerque a algún pez estas luces se apaguen para no dañar a los peces ya que ellos no están acostumbrados a ella. El prototipo de submarino que ellos realizaron tendría un impacto positivo en lo que es la preservación de la vida marina, de esta manera cuando se realicen exploraciones no se estaría dañando a los peces ya las diferentes especies marinas.  Lo que más les gusto de este proyecto fue unir todas las ideas que tuvieron en su grupo en una sola gran idea creativa. '
                 },
             ],
 
-            articulos: [
-                {
-                    titulo: 'Titulo Articulo',
-                    imagen: 'url de la imagen /noticias/<equipo_x_y>.jpg',
-                    imagenDesc: 'descripción de la imágen',
-                    articulo: 'Lorem ipsum'
-                },
-            ]
 
         },
         {
