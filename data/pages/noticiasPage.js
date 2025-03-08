7e9f7e3a094299960bb19db1e4b3f244951d12dc
KHasbun <khalidhasbun13@gmail.com>
2025-03-08 11:31:15 -0600

    ya

--- data/pages/noticiasPage.js
+++ data/pages/noticiasPage.js
@@ -1,128 +1,134 @@
 export const noticiasPage = {
-    noticias: [
+  noticias: [
+    {
+      titulo: "Equipo -1",
+      articulos: [
         {
-            titulo: 'Equipo -1',
-            articulos: [
-                {
-                    titulo: 'Titulo Articulo',
-                    imagen: 'noticias/imgs-1/imagenx.jpg',
-                    imagenDesc: 'descripción de la imágen',
-                    articulo: 'Lorem ipsum'
-                },
-                {
-                    titulo: 'Titulo Articulo',
-                    imagen: 'url de la imagen /noticias/<equipo_x_y>.jpg',
-                    imagenDesc: 'descripción de la imágen',
-                    articulo: 'Lorem ipsum'
-                },
-                {
-                    titulo: 'Titulo Articulo',
-                    imagen: 'url de la imagen /noticias/<equipo_x_y>.jpg',
-                    imagenDesc: 'descripción de la imágen',
-                    articulo: 'Lorem ipsum'
-                },
-            ],
+          titulo: "Mega Bulls",
+          imagen: "noticias/image-1/megaBulls.jpg",
+          imagenDesc:
+            "El proyecto busca interpretar los sonidos de los corales para evaluar su bienestar y explorar posibles beneficios médicos, ya que pueden vivir más de 500 años. Uno de los principales retos fue adquirir el hidrófono, un equipo disponible solo en EE.UU., enfrentando dificultades bancarias para su compra.",
+          articulo:
+            "Durante el desarrollo, se realizó un viaje de estudio a Tela, donde el equipo exploró ecosistemas coralinos y profundizó en su investigación. El objetivo es analizar los sonidos de los corales con un chip Oteo: más sonido indica un ecosistema saludable, menos sonido sugiere deterioro. Entre las amenazas identificadas están el cambio climático y la pesca.",
         },
         {
-            titulo: 'Equipo -2',
-            articulos: [
-                {
-                    titulo: 'Titulo Articulo',
-                    imagen: 'url de la imagen /noticias/<equipo_x_y>.jpg',
-                    imagenDesc: 'descripción de la imágen',
-                    articulo: 'Lorem ipsum'
-                },
-                {
-                    titulo: 'Titulo Articulo',
-                    imagen: 'url de la imagen /noticias/<equipo_x_y>.jpg',
-                    imagenDesc: 'descripción de la imágen',
-                    articulo: 'Lorem ipsum'
-                },
-                {
-                    titulo: 'Titulo Articulo',
-                    imagen: 'url de la imagen /noticias/<equipo_x_y>.jpg',
-                    imagenDesc: 'descripción de la imágen',
-                    articulo: 'Lorem ipsum'
-                },
-            ]
+          titulo: "TechTigers",
+          imagen: "noticias/image-1/techTigers.jpg",
+          imagenDesc:
+            "El equipo Tech Tigers desarrolló un dron marino diseñado para capturar imágenes de los corales y analizar la calidad del agua. Su principal ventaja es que puede permanecer sumergido 24 horas, facilitando el estudio de los ecosistemas marinos, ya que los buzos solo pueden estar aproximadamente 30 minutos bajo el agua. Además, su precisión evita dañar los corales, que son muy delicados.",
+          articulo:
+            "Inicialmente, el proyecto utilizaba respirometría, pero seis semanas antes del evento lo cambiaron tras recibir asesoría de un biólogo en Telamar, buscando una solución más simple pero con mayor impacto. Uno de los mayores aprendizajes fue que un proyecto exitoso no necesita ser costoso: su idea inicial costaba miles de dólares, pero con materiales reciclados lograron reducirlo a 500-400 dólares, manteniendo su efectividad y accesibilidad.",
         },
         {
-            titulo: 'Equipo -3',
-            articulos: [
-                {
-                    titulo: 'Titulo Articulo',
-                    imagen: 'url de la imagen /noticias/<equipo_x_y>.jpg',
-                    imagenDesc: 'descripción de la imágen',
-                    articulo: 'Lorem ipsum'
-                },
-                {
-                    titulo: 'Titulo Articulo',
-                    imagen: 'url de la imagen /noticias/<equipo_x_y>.jpg',
-                    imagenDesc: 'descripción de la imágen',
-                    articulo: 'Lorem ipsum'
-                },
-                {
-                    titulo: 'Titulo Articulo',
-                    imagen: 'url de la imagen /noticias/<equipo_x_y>.jpg',
-                    imagenDesc: 'descripción de la imágen',
-                    articulo: 'Lorem ipsum'
-                },
-            ]
+          titulo: "Hawks Mechanics",
+          imagen: "noticias/image-1/hawksMechanics.jpg",
+          imagenDesc:
+            "El proyecto consiste en un *sistema de cuadrantes* que analiza el agua para *localizar corales* y determinar si las condiciones submarinas son aptas para su desarrollo. Actualmente, no hay registros exactos de su ubicación, por lo que esta herramienta facilitaría su monitoreo.  ",
+          articulo:
+            "Lo que más disfrutaron fue aprender sobre *batimetría*, la ciencia que estudia el fondo marino y su formación.   El mayor desafío fue *unificar ideas dentro del equipo*, ya que al inicio cada miembro tenía enfoques distintos. Sin embargo, lograron combinar sus propuestas en un proyecto que representara su visión en común.",
+        },
+      ],
+    },
+    {
+      titulo: "Equipo -2",
+      articulos: [
+        {
+          titulo: "Titulo Articulo",
+          imagen: "url de la imagen /noticias/<equipo_x_y>.jpg",
+          imagenDesc: "descripción de la imágen",
+          articulo: "Lorem ipsum",
         },
         {
-            titulo: 'Equipo -4',
-            articulos: [
-                {
-                    titulo: 'Titulo Articulo',
-                    imagen: 'url de la imagen /noticias/<equipo_x_y>.jpg',
-                    imagenDesc: 'descripción de la imágen',
-                    articulo: 'Lorem ipsum'
-                },
-            ]
+          titulo: "Titulo Articulo",
+          imagen: "url de la imagen /noticias/<equipo_x_y>.jpg",
+          imagenDesc: "descripción de la imágen",
+          articulo: "Lorem ipsum",
         },
         {
-            titulo: 'Equipo -5',
-            articulos: [
-                {
-                    titulo: 'Titulo Articulo',
-                    imagen: 'url de la imagen /noticias/<equipo_x_y>.jpg',
-                    imagenDesc: 'descripción de la imágen',
-                    articulo: 'Lorem ipsum'
-                },
-            ]
+          titulo: "Titulo Articulo",
+          imagen: "url de la imagen /noticias/<equipo_x_y>.jpg",
+          imagenDesc: "descripción de la imágen",
+          articulo: "Lorem ipsum",
+        },
+      ],
+    },
+    {
+      titulo: "Equipo -3",
+      articulos: [
+        {
+          titulo: "Titulo Articulo",
+          imagen: "url de la imagen /noticias/<equipo_x_y>.jpg",
+          imagenDesc: "descripción de la imágen",
+          articulo: "Lorem ipsum",
         },
         {
-            titulo: 'Equipo -6',
-            articulos: [
-                {
-                    titulo: 'Titulo Articulo',
-                    imagen: 'url de la imagen /noticias/<equipo_x_y>.jpg',
-                    imagenDesc: 'descripción de la imágen',
-                    articulo: 'Lorem ipsum'
-                },
-            ]
+          titulo: "Titulo Articulo",
+          imagen: "url de la imagen /noticias/<equipo_x_y>.jpg",
+          imagenDesc: "descripción de la imágen",
+          articulo: "Lorem ipsum",
         },
         {
-            titulo: 'Equipo -7',
-            articulos: [
-                {
-                    titulo: 'Titulo Articulo',
-                    imagen: 'url de la imagen /noticias/<equipo_x_y>.jpg',
-                    imagenDesc: 'descripción de la imágen',
-                    articulo: 'Lorem ipsum'
-                },
-            ]
+          titulo: "Titulo Articulo",
+          imagen: "url de la imagen /noticias/<equipo_x_y>.jpg",
+          imagenDesc: "descripción de la imágen",
+          articulo: "Lorem ipsum",
         },
+      ],
+    },
+    {
+      titulo: "Equipo -4",
+      articulos: [
         {
-            titulo: 'Equipo -8',
-            articulos: [
-                {
-                    titulo: 'Titulo Articulo',
-                    imagen: 'url de la imagen /noticias/<equipo_x_y>.jpg',
-                    imagenDesc: 'descripción de la imágen',
-                    articulo: 'Lorem ipsum'
-                },
-            ]
+          titulo: "Titulo Articulo",
+          imagen: "url de la imagen /noticias/<equipo_x_y>.jpg",
+          imagenDesc: "descripción de la imágen",
+          articulo: "Lorem ipsum",
         },
-    ]
-}
+      ],
+    },
+    {
+      titulo: "Equipo -5",
+      articulos: [
+        {
+          titulo: "Titulo Articulo",
+          imagen: "url de la imagen /noticias/<equipo_x_y>.jpg",
+          imagenDesc: "descripción de la imágen",
+          articulo: "Lorem ipsum",
+        },
+      ],
+    },
+    {
+      titulo: "Equipo -6",
+      articulos: [
+        {
+          titulo: "Titulo Articulo",
+          imagen: "url de la imagen /noticias/<equipo_x_y>.jpg",
+          imagenDesc: "descripción de la imágen",
+          articulo: "Lorem ipsum",
+        },
+      ],
+    },
+    {
+      titulo: "Equipo -7",
+      articulos: [
+        {
+          titulo: "Titulo Articulo",
+          imagen: "url de la imagen /noticias/<equipo_x_y>.jpg",
+          imagenDesc: "descripción de la imágen",
+          articulo: "Lorem ipsum",
+        },
+      ],
+    },
+    {
+      titulo: "Equipo -8",
+      articulos: [
+        {
+          titulo: "Titulo Articulo",
+          imagen: "url de la imagen /noticias/<equipo_x_y>.jpg",
+          imagenDesc: "descripción de la imágen",
+          articulo: "Lorem ipsum",
+        },
+      ],
+    },
+  ],
+};
