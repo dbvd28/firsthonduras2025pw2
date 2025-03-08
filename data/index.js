7c0cd6ed1cc9b90d86303f76f1a82fbe1b1826b7
Orlando Betancourth <orlando@uledger.io>
2025-03-08 09:21:27 -0600

    feat: pages data setup

--- data/index.js
+++ data/index.js
@@ -1,19 +1,32 @@
+import { indexPage } from "./pages/indexPage";
+import { contactUsPage } from "./pages/contactUsPage";
+import { noticiasPage } from "./pages/noticiasPage";
+import { galeriaPage } from "./pages/galeriaPage";
+
 
 const commonData = {
     navigation: [
-        {url:'/index.html', label:'Inicio'},
-        {url:'/galeria.html', label:'Galería'},
-        {url:'/noticias.html', label:'Noticias'},
-        {url:'/contactus.html', label:'Acerca de FLL'},
+        { url: '/index.html', label: 'Inicio' },
+        { url: '/galeria.html', label: 'Galería' },
+        { url: '/noticias.html', label: 'Noticias' },
+        { url: '/contactus.html', label: 'Acerca de FLL' },
     ]
 }
 
 export const getPageContext = (pagePath) => {
     let pageData = {};
     switch (pagePath) {
-        case  '/index.html':
+        case '/index.html':
+            pageData = indexPage
             break;
         case '/contactus.html':
+            pageData = contactUsPage
+            break;
+        case '/noticias.html':
+            pageData = noticiasPage
+            break;
+        case '/galeria.html':
+            pageData = galeriaPage
             break;
         default:
             break;
---
+++ data/pages/indexPage.js
@@ -0,0 +1,16 @@
+export const indexPage = {
+    indexSummary:[
+        {
+          imgSrc: 'https://picsum.photos/id/20/640/480',
+          imgDsc: 'Imágen de Referencia 1'
+        },
+        {
+            imgSrc: 'https://picsum.photos/id/21/640/480',
+            imgDsc: 'Imágen de Referencia 2'
+        },
+        {
+            imgSrc: 'https://picsum.photos/id/22/640/480',
+            imgDsc: 'Imágen de Referencia 3'
+          }
+      ]
+}
