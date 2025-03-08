ef170dbb80e09e2933ee320a70403640e84bad3a
LDIAZ07 <louisdays845@gmail.com>
2025-03-08 11:23:54 -0600

    feat: noticias equipo 3

--- data/pages/noticiasPage.js
+++ data/pages/noticiasPage.js
@@ -42,10 +42,24 @@ export const noticiasPage = {
             titulo: 'Equipo -3',
             articulos: [
                 {
-                    titulo: 'Titulo Articulo',
-                    imagen: 'url de la imagen /noticias/<equipo_x_y>.jpg',
-                    imagenDesc: 'descripción de la imágen',
-                    articulo: 'Lorem ipsum'
+                    titulo: 'Winded Innovators',
+                    imagen: 'noticias/img-3/Winded-Innovator-G3.jpeg',
+                    imagenDesc: 'El organizador',
+                    articulo: 'Winged Innovators ¿De qué trata? Se trata de un robot autónomo diseñado para la recolección de contaminantes en cuerpos de agua como océanos, embalses y ríos. El robot se sumerge en el agua y está equipado con una cinta transportadora que incorpora un sensor de clasificación, el cual distingue entre organismos vivos y desechos. Una vez que los objetos pasan por la cinta, un motor actuador separa los peces de los desechos, almacenándolos para su posterior tratamiento y reciclaje. Dificultades El equipo se basó en un prototipo de robot previamente desarrollado, pero este no cumplía con los requerimientos técnicos específicos. Fue necesario realizar modificaciones estructurales, como la ampliación de la cinta transportadora, que originalmente solo disponía de un carril, para permitir una clasificación más eficiente. Lo que más te gustó Lo más destacado fue la implementación de una solución innovadora para un problema ambiental, lo que requirió un alto nivel de creatividad y adaptación técnica.'
+                },
+
+                {
+                    titulo: 'Asta Eagles',
+                    imagen: 'noticias/img-3-AST_EAGLES_1.jpg',
+                    imagenDesc: 'Juegos representativos',
+                    articulo: 'Ast Eagles ¿De qué trata? Este proyecto tiene como objetivo abordar problemas ambientales en los océanos, como la proliferación de especies invasoras, la contaminación por plásticos y la alteración de las cadenas tróficas. La meta es desarrollar programas educativos dirigidos a docentes, con el fin de sensibilizar a los estudiantes sobre el impacto de la contaminación. El programa incluye módulos interactivos basados en videojuegos, desarrollados con la plataforma Microsoft MakeCode, que pueden ejecutarse en dispositivos como Game Boy o en línea. Estos juegos tienen el propósito de enseñar y concientizar a los estudiantes sobre la preservación ambiental.'
+                },
+
+                {
+                    titulo: 'FootSteps',
+                    imagen: 'noticias/img-3/FOOTSTEPS_1.jpg',
+                    imagenDesc: 'Dron Fantasma',
+                    articulo: 'Este proyecto consiste en el desarrollo de un dron submarino autónomo destinado a la extracción de "redes fantasma", redes de pesca abandonadas que representan una amenaza significativa para la fauna marina y los ecosistemas de corales. Las redes fantasma son en su mayoría restos de material pesquero, muchas veces descartados por las grandes industrias pesqueras, que no toman las precauciones necesarias para recuperar estos residuos. La contaminación se origina cuando las redes quedan atrapadas en el fondo marino, y los pescadores prefieren cortarlas y dejarlas sumergidas, en lugar de proceder con su recuperación.'
                 },
             ]
         },
