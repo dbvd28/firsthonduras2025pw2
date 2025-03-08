f84369013c0c3c098db4dbe0429962568aed75f6
LuisGustavoRubi <161526218+LuisGustavoRubi@users.noreply.github.com>
2025-03-08 11:30:15 -0600

    imagenesGrupo7

--- data/pages/noticiasPage.js
+++ data/pages/noticiasPage.js
@@ -106,12 +106,24 @@ export const noticiasPage = {
             titulo: 'Equipo -7',
             articulos: [
                 {
-                    titulo: 'Titulo Articulo',
-                    imagen: 'url de la imagen /noticias/<equipo_x_y>.jpg',
+                    titulo: 'ACTON SUFFERS',
+                    imagen: '/images/image_7_1.jpg',
                     imagenDesc: 'descripción de la imágen',
-                    articulo: 'Lorem ipsum'
+                    articulo: 'El océano está en peligro. La contaminación y el cambio climático están destruyendo hábitats marinos y afectando a muchas especies que dependen de ellos para sobrevivir.'
                 },
-            ]
+                {
+                    titulo: 'SEA STARS',
+                    imagen: '/images/image_7_6.jpg',
+                    imagenDesc: 'descripción de la imágen',
+                    articulo: 'Los corales son más que rocas bonitas en el océano. Son seres vivos que forman los arrecifes, el hogar de miles de especies marinas. Sin ellos, muchos peces, tortugas y otros animales perderían su refugio y fuente de alimento'
+                },
+                {
+                    titulo: 'ROBO GENIUS',
+                    imagen: '/images/image_7_3.jpg',
+                    imagenDesc: 'descripción de la imágen',
+                    articulo: 'En Robo Genius, combinamos nuestra pasión por la robótica y la programación para desarrollar soluciones innovadoras en el monitoreo oceánico. Nuestro proyecto se basa en el uso de cámaras especializadas para la localización de cúmulos de peces, ayudando en la conservación de especies, el análisis de su salud y el estudio de sus patrones migratorios.'
+                },
+            ],
         },
         {
             titulo: 'Equipo -8',
