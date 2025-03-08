ac1c5963335e4c8fb7cd92ada7af116ff95ed6a8
FabyMencia <poncefabiola939@gmail.com>
2025-03-08 10:57:40 -0600

    Se agrega la carpeta de imagenes del grupo 6 y se modifica el articulo 1

--- data/pages/noticiasPage.js
+++ data/pages/noticiasPage.js
@@ -75,12 +75,31 @@ export const noticiasPage = {
             titulo: 'Equipo -6',
             articulos: [
                 {
+                    titulo: 'Rest Wreck',
+                    imagen: 'public/imagenes_grupo6/AST_EAGLES_JUNIOR.jpeg',
+                    imagenDesc: 'Estudiantes de AST Eagles Junior',
+                    articulo: 'Los estudiantes de AST se basaron su proyecto en el arrecife de coral en Tela, explican que el propósito de su proyecto es toman un poco de larva que se encuentra de corales sanos y se ponen dentro de corales que están por morir, esto lo que hace es que el coral que este por morir pueda volver a regenerarse. Esto es muy útil ya que así podríamos combatir la extinción o daño que reciben los corales debido a los turistas.'
+                },
+            ],
+
+            articulos: [
+                {
+                    titulo: 'Titulo Articulo',
+                    imagen: 'url de la imagen /noticias/<equipo_x_y>.jpg',
+                    imagenDesc: 'descripción de la imágen',
+                    articulo: 'Lorem ipsum'
+                },
+            ],
+
+            articulos: [
+                {
                     titulo: 'Titulo Articulo',
                     imagen: 'url de la imagen /noticias/<equipo_x_y>.jpg',
                     imagenDesc: 'descripción de la imágen',
                     articulo: 'Lorem ipsum'
                 },
             ]
+
         },
         {
             titulo: 'Equipo -7',
