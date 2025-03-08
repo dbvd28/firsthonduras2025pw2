74e2b79edc9dcfa839da977be020edadfc4ea245
LuisGustavoRubi <161526218+LuisGustavoRubi@users.noreply.github.com>
2025-03-08 10:47:55 -0600

    NoticiasHbs

--- data/pages/noticiasPage.js
+++ data/pages/noticiasPage.js
@@ -1,3 +1,15 @@
 export const noticiasPage = {
-
+    noticias: [
+        {
+            titulo: 'Titulo o Nombre del Euipo',
+            articulos: [
+                {
+                    titulo: 'Titulo Articulo',
+                    imagen: 'url de la imagen /noticias/<equipo_x_y>.jpg',
+                    imagenDesc: 'descripción de la imágen',
+                    articulo: 'Lorem ipsum'
+                },
+            ]
+        },
+    ]
 }
